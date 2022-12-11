05226e6f1a36995979ea7da48aaef6447ecd6bd1
Talha_Altair <cooltalha27@gmail.com>
2022-12-12 00:30:22 +0530

    Updates for 2022-12-12 00:30:22

--- handlers/db_handler.py
+++ handlers/db_handler.py
@@ -1,1 +1,24 @@
 from connections import db
+
+product_data_coll = db['product-data']
+
+
+def get_product(product_id):
+
+    all_keywords = product_id.split(" ")
+
+    regex_exp = "(" + ")|(".join(all_keywords) + ")"
+
+    data = product_data_coll.find({'$or': [
+        {"product_name": {"$regex": regex_exp}}
+    ]})
+
+    data = list(data)
+
+    for i in data:
+
+        if "_id" in i:
+
+            del i["_id"]
+
+    return list(data)
--- routes/api.py
+++ routes/api.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from handlers import db_handler
 
 api = Blueprint('base', __name__)
 
@@ -9,3 +10,15 @@ def home():
     return jsonify(
         {"message": "Welcome to Flood Backend"}
     )
+
+
+@api.route("/product", methods=['GET', 'POST'])
+def product():
+
+    keyword = request.json.get('product')
+
+    product_data = db_handler.get_product(keyword)
+
+    return jsonify({
+        "data": product_data
+    })
